86d3acc23f398bba8698e3cfcd7832b9c04d1ead
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-02-08 12:14:22 +0100

    Updated Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,53 @@
 <template>
   <div class="home">
-    <h1>Movies</h1>
+    <h1>Now playing</h1>
+    <div class="home__container">
+      <Card
+        v-for="movie of movies"
+        :key="movie.id"
+        :id="movie.id"
+        :title="movie.title"
+        :imageUrl="movie.poster_path"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import Card from "../components/Card";
+
 export default {
-  name: 'Home',
-}
+  name: "Home",
+  components: {
+    Card,
+  },
+  data() {
+    return {
+      movies: [],
+    };
+  },
+  created() {
+    const base = "https://api.themoviedb.org/3/movie/now_playing";
+    const API = `?api_key=${process.env.VUE_APP_API_KEY}`;
+
+    fetch(base + API)
+      .then((res) => res.json())
+      .then((data) => (this.movies = data.results))
+      .catch((e) => console.log(e));
+  },
+};
 </script>
 
 <style scoped>
 .home {
   width: 100%;
+  text-align: center;
+}
+
+.home__container {
+  margin: auto;
+  width: 60%;
   display: flex;
-  flex-direction: column;
-  align-items: center;
+  flex-wrap: wrap;
 }
 </style>
