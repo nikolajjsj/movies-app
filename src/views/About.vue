1940248f17978dd06527e510aa83c8efba5d2ad5
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-02-08 12:14:06 +0100

    Basic app info

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,34 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About App</h1>
+    <h2>Vue.js app displaying data from:</h2>
+    <a
+      class="about__link"
+      href="https://developers.themoviedb.org"
+      target="_blank"
+      ><strong>https://developers.themoviedb.org</strong>
+    </a>
+    <h2>Created by Nikolaj Johannes Skole Jensen</h2>
+    <a
+      class="about__link"
+      href="https://github.com/nikolajjsj/movies-app"
+      target="_blank"
+      ><strong>https://github.com/nikolajjsj/movies-app</strong>
+    </a>
   </div>
 </template>
+
+<style scoped>
+.about {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.about__link {
+  text-decoration: none;
+  color: inherit;
+}
+</style>
