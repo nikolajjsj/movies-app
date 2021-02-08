0ed7ef14c304fde54128431101433d9f9b543a9e
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-02-08 16:00:19 +0100

    Updated styling

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="about">
-    <h1>About App</h1>
-    <h2>Vue.js app displaying data from:</h2>
+    <h1 class="about__title">About App</h1>
+    <h3 class="about__subtitle">Vue.js app displaying data from:</h3>
     <a
       class="about__link"
       href="https://developers.themoviedb.org"
       target="_blank"
       ><strong>https://developers.themoviedb.org</strong>
     </a>
-    <h2>Created by Nikolaj Johannes Skole Jensen</h2>
+    <h3 class="about__subtitle">Created by Nikolaj Johannes Skole Jensen</h3>
     <a
       class="about__link"
       href="https://github.com/nikolajjsj/movies-app"
@@ -20,11 +20,22 @@
 
 <style scoped>
 .about {
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  width: 80%;
+  position: absolute;
+  top: 60px;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+  text-align: center;
+}
+
+.about__subtitle {
+  margin: 1rem;
+}
+
+.about__subtitle {
+  margin-top: 2rem;
 }
 
 .about__link {
